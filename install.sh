1db03214470b47bcb8e44a349da8ba6e31007e8b
TN3rd <TN3rd@protonmail.com>
2019-03-02 12:13:01 +0100

    Repaired the installer to work as intended for non-superuser install

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 if [ "$EUID" -ne 0 ]; then
 	mv Adwaita-dark-OSX/ $HOME/.themes
 	mv Adwaita-light-OSX/ $HOME/.themes
+	cd $HOME/.themes
+	mkdir Adwaita-dark-OSX/
+	mv gtk-2.0 Adwaita-dark-OSX/
+	mv gtk-3.0 Adwaita-dark-OSX/
 	echo "Installed as normal user, the install directory is $HOME/.themes"
 	exit
 else
