4d381295e1d9af07978cbc1937d61ce95156a7c8
TN3rd <TN3rd@protonmail.com>
2019-03-02 08:20:49 +0100

    Unified install scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-mv Adwaita-dark-OSX/ $HOME/.themes
-mv Adwaita-light-OSX/ $HOME/.themes
+if [ "$EUID" -ne 0 ]; then
+	mv Adwaita-dark-OSX/ $HOME/.themes
+	mv Adwaita-light-OSX/ $HOME/.themes
+	echo "Installed as normal user, the install directory is $HOME/.themes"
+	exit
+else
+	mv Adwaita-dark-OSX/ /usr/share/themes
+	mv Adwaita-light-OSX/ /usr/share/themes
+	echo "Installed as superuser, the install directory is /usr/share/themes"
+	exit
+fi
+
+
